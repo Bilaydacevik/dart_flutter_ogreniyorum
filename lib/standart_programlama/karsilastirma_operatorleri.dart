fd0bbc912b25fe952afa7121ba00595540c4be0a
Büşra İlayda Çevik-Köken <bilaydacevik@hotmail.com>
2025-11-24 20:23:56 +0300

    ders 2

---
+++ lib/standart_programlama/karsilastirma_operatorleri.dart
@@ -0,0 +1,13 @@
+void main() {
+  int a = 40;
+  int b = 30;
+
+  int x = 34;
+  int y= 80;
+
+  print (" a == b : ${a==b}");
+  print (" a != b : ${a!=b}");  //eşit değil mi sorusu
+  print (" a > b : ${a>b}");
+  print (" a < b : ${a<b}");
+  print (" a <= b : ${a<=b}");
+}

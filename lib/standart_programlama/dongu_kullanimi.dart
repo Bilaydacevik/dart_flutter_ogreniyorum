ebbd7724b0c613e8e1fcfc51c05c2d71605948f3
Büşra İlayda Çevik-Köken <bilaydacevik@hotmail.com>
2025-11-25 21:08:16 +0300

    donguler

--- lib/standart_programlama/dongu_kullanimi.dart
+++ lib/standart_programlama/dongu_kullanimi.dart
@@ -3,4 +3,29 @@ void main() {
     print ("Döngü 1 : $i");
   }
 
+  for (var x= 10; x<21; x+=5) {  // 5'er artsın   -= azalması için kullanılır.
+    print ("Döngü 2 : $x");
+  }
+  var sayac = 1;
+  while(sayac<4) {
+    print("Döngü 4: $sayac");
+    sayac = sayac+1 ;  // sayac+=1 ya da sayac++ denebilir.
+  }
+
+  for (var a= 1; a<4; a++) {
+    if(a==3){
+      break;
+    }
+    print("Döngü 5 : $a");
+  }
+
+
+
+  for (var a = 1; a < 6; a++) {
+    if (a == 3) {
+      continue; // 3'ü atla
+    }
+    print("Değer: $a");
+  }
+
 }

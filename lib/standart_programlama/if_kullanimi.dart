7fe8262f060fddb3804cf8a6036964862e83535a
Büşra İlayda Çevik-Köken <bilaydacevik@hotmail.com>
2025-11-25 14:01:17 +0300

    ders 2 or ve and

---
+++ lib/standart_programlama/if_kullanimi.dart
@@ -0,0 +1,17 @@
+void main() {
+  int yas = 19;
+  String isim = "Kimim ben";
+
+  if (yas >= 18) {
+    print("Reşitsiniz");
+  } else {
+    print("Reşit değilsiniz.");
+  }
+  if (isim == "Ahmet") {
+    print("Merhaba Ahmet");
+  }else if ( isim == "Nisa") {
+    print("Merhaba Nisa");
+  }else{
+    print("Tanınmayan kişi");
+  }
+}
